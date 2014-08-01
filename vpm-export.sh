e5663735fdedb295bfd94e21f130273364c4414a
Ismail Badawi <ismail@badawi.io>
2014-08-01 14:36:59 -0400

    Add vim-exchange.

--- vpm-export.sh
+++ vpm-export.sh
@@ -3,6 +3,7 @@ vpm install git@github.com:sjl/clam.vim
 vpm install git@github.com:ervandew/supertab
 vpm install git@github.com:p8952/vim-colors-wombat
 vpm install git@github.com:tpope/vim-commentary
+vpm install git@github.com:tommcdo/vim-exchange
 vpm install git@github.com:tpope/vim-fugitive
 vpm install git@github.com:michaeljsmith/vim-indent-object
 vpm install git@github.com:groenewege/vim-less
