11b8802818a0f8b7e393f15e1a55bcff0e7e3640
Ismail Badawi <ismail@badawi.io>
2014-09-17 16:26:34 -0400

    Add vim-go.

--- vpm-export.sh
+++ vpm-export.sh
@@ -6,6 +6,7 @@ vpm install git@github.com:tpope/vim-commentary
 vpm install git@github.com:ap/vim-css-color
 vpm install git@github.com:tommcdo/vim-exchange
 vpm install git@github.com:tpope/vim-fugitive
+vpm install git@github.com:fatih/vim-go
 vpm install git@github.com:michaeljsmith/vim-indent-object
 vpm install git@github.com:groenewege/vim-less
 vpm install git@github.com:tpope/vim-rails
