a30da444fba2ebeb57e942861b869a5f22de98d9
Ismail Badawi <ismail@badawi.io>
2014-12-09 10:38:19 -0500

    Add a script for linking everything.
    Along the way, make vpm a submodule & make vpm-export.sh executable.

---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+this_dir=$(cd "$(dirname "$0")" && pwd)
+
+function link {
+  local source="$1"
+  local target="$2"
+  if [ -e $target ]; then
+    echo "$target exists, backing up to $target.old just in case"
+    mv $target $target.old
+  fi
+  ln -vs $source $target
+}
+
+function init_vim {
+  echo "Installing pathogen..."
+  mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+  curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  echo "Installing vpm..."
+  git submodule update --init --remote vpm
+  mkdir -p $HOME/bin
+  link $this_dir/vpm/vpm $HOME/bin/vpm
+  echo "Installing vim plugins..."
+  ./vpm-export.sh
+}
+
+function link_dotfiles {
+  local symlinks=".bashrc .ctags .gitconfig .githelpers .inputrc .vimrc"
+  for symlink in $symlinks; do
+    link $this_dir/$symlink $HOME/$symlink
+  done
+}
+
+function main {
+  link_dotfiles
+  init_vim
+}
+
+main
